80e7c52297029f86ce8b1197fb18c27359a2c5b0
EmAyyTee <hanzoswitchmail@gmail.com>
2025-01-15 19:45:21 +0100

    Trying to add walls

---
+++ ProceduralGeneration/WallGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HanzoSwitch on 1/15/2025.
+//
+
+#include "WallGenerator.h"
---
+++ ProceduralGeneration/WallGenerator.h
@@ -0,0 +1,16 @@
+//
+// Created by HanzoSwitch on 1/15/2025.
+//
+
+#ifndef WALLGENERATOR_H
+#define WALLGENERATOR_H
+
+
+
+class WallGenerator {
+
+};
+
+
+
+#endif //WALLGENERATOR_H
